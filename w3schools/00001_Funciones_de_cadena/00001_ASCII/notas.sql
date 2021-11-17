3107e52474f2a556f3338e3be8c5bb31d16f94ce
caleman9791 <caleman9791@protonmail.com>
2021-11-17 01:32:59 -0400

    ok

---
+++ w3schools/00001_Funciones_de_cadena/00001_ASCII/notas.sql
@@ -0,0 +1,14 @@
+-- Funciones y operadores de cadenas
+
+-- Devuelve el valor numérico del carácter situado  más a la izquierda de la cadena str. 
+-- Devuelve 0 si str es la cadena vacía. Devuelve NULL si str es NULL.
+
+
+SELECT ASCII('2');
+-- -> 50
+
+SELECT ASCII(2);
+-- -> 50
+
+SELECT ASCII('dx');
+-- -> 100
---
+++ w3schools/00001_Funciones_de_cadena/00002_CHAR_LENGTH/notas.sql
@@ -0,0 +1,3 @@
+
+-- La función CHAR_LENGTH () devuelve la longitud de una cadena (en caracteres).
+SELECT CHAR_LENGTH("SQL Tutorial") AS LengthOfString; 
